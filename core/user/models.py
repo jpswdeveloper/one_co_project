dcd383f89b8724a3c70f84802bc75c65be77d2a6
Tedy Girma <girmatedy48@gmail.com>
2024-02-06 16:06:56 +0300

    update user model to abstarct base user manager

--- core/user/models.py
+++ core/user/models.py
@@ -1,12 +1,25 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 import uuid
 from role.models import Role
 
 # Create your models here.
 
 
-class User(AbstractUser):
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email field must be set")
+
+        email = self.normalize_email(email=email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     age = models.PositiveIntegerField(null=True, blank=True)
     phone = models.CharField(max_length=20, null=True, blank=True)
@@ -18,6 +31,8 @@ class User(AbstractUser):
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     roleId = models.ForeignKey(Role, on_delete=models.CASCADE, null=False, blank=False)
+
+    objects = UserManager()
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
